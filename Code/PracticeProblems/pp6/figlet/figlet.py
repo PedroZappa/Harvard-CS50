b7ad70e589cf82af25c0a3c5bc23546cf12de69f
PedroZappa <pedrogzappa@gmail.com>
2023-05-26 00:19:57 +0100

    figlet.py : implemented main() refactored codeinto several functions, implemented random color

--- Code/PracticeProblems/pp6/figlet/figlet.py
+++ Code/PracticeProblems/pp6/figlet/figlet.py
@@ -1,11 +1,43 @@
 import sys, random
 from pyfiglet import Figlet
+from colored import fg, bg, attr
 
-isRandom = None
+isRandom = True
 figlet = Figlet()
 fonts = figlet.getFonts()
 
-# Usage Guide Printer
+def main():
+    # Check arguments / check flags / set mode
+    ## Random Mode
+    if len(sys.argv) == 1:
+        # Pick random font
+        isRandom = True
+    ## Dictionary Mode
+    elif sys.argv[1] == "-d" or sys.argv[1] == "--dict":
+        print_dict()
+        exit(1)
+    ## Usage Guide Help Mode
+    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        print_help()
+        exit(2)
+    elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+        # set user input font
+        isRandom = False
+    else:
+        print("Invalid usage.\nRun with -h for help.")
+        sys.exit(3)
+        
+    # Set font
+    font = set_font()
+    
+    # Get user input
+    text = input("Input: ")
+    
+    # Print to the terminal
+    print_font(text, font)
+    exit(0)
+
+
 def print_help():
     print("Zedro's Figlet Writer Usage Guide: ||")
     print("                                   ||")
@@ -21,57 +53,49 @@ def print_help():
     print("./figlet.py --help                 ||")
     print("            -h                     ||")
 
-# Check arguments / set mode
-## Random Mode
-if len(sys.argv) == 1:
-    # Pick random font
-    isRandom = True
-# Check flags
-## Dictionary Mode
-elif sys.argv[1] == "-d" or sys.argv[1] == "--dict":
+
+def print_dict():
     # Print font list
-    print("Dictionary of Figlet Fonts:\n")
-    
-    cols = 10
-    rows = len(fonts) // cols + (1 if len(fonts) % cols > 0 else 0)
-    for row in range(rows):
-        for col in range(cols):
-            index = row * cols + col
-            if index < len(fonts):
-                print(f'{fonts[index]: <15}', end="")
-        print()
-    exit(1)
-## Usage Guide Help Mode
-elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    print_help()
-    exit(2)
-elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
-    # set user input font
-    isRandom = False
-else:
-    print("Invalid usage.\nRun with -h for help.")
-    sys.exit(3)
+        print(f"{fg(random.randint(0, 255))}Dictionary of Figlet Fonts:\n")
+        cols = 10
+        rows = len(fonts) // cols + (1 if len(fonts) % cols > 0 else 0)
+        for row in range(rows):
+            for col in range(cols):
+                index = row * cols + col
+                if index < len(fonts):
+                    print(f'{fg(random.randint(0, 255))}{fonts[index]: <15}', end="")
+            print("")
 
-# Set Figlet options
-if isRandom == False:
-    try:
-        # Set user defined font
-        font = sys.argv[2]
+def set_font():
+    # Set Figlet options
+    if isRandom == False:
+        try:
+            # Set user defined font
+            font = sys.argv[2]
+            figlet.setFont(font=font)
+        except:
+            # handle invalid font
+            print("Invalid font.\nRun with -h for help.")
+            sys.exit(4)
+    else:
+        isRandom == True
+        # Get random font
+        font = random.choice(figlet.getFonts())
+        # set random font
         figlet.setFont(font=font)
-    except:
-        # handle invalid font
-        print("Invalid font.\nRun with -h for help.")
-        sys.exit(4)
-else:
-    # Get random font
-    font = random.choice(figlet.getFonts())
-    # set random font
-    figlet.setFont(font=font)
-    
-# Get user input
-text = input("Input: ")
+    return font
 
-# Print font meta
-print(f"\nFont: {font}")
-# Print font
-print(f"Output:\n\n{figlet.renderText(text)}")
+
+def print_font(text, font):
+    # Print font meta
+    print(f"Font: {font}")
+    # Print font
+    # if random is true print random color
+    if isRandom is True:
+        print("Output:\n\n")
+        print(f"{fg(random.randint(0, 255))}{figlet.renderText(text)}")
+    else:
+        print(f"_Output:\n\n{figlet.renderText(text)}")
+
+# Run
+main()
