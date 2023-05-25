2b22579837d9ff3cfe4257f011e84d281cc1b110
PedroZappa <pedrogzappa@gmail.com>
2023-05-25 23:34:54 +0100

    figlet.py : flags for Usage guide, Dictionary of fonts

--- Code/PracticeProblems/pp6/figlet/figlet.py
+++ Code/PracticeProblems/pp6/figlet/figlet.py
@@ -5,16 +5,52 @@ isRandom = None
 figlet = Figlet()
 fonts = figlet.getFonts()
 
-# Check for arguments
+# Usage Guide Printer
+def print_help():
+    print("Zedro's Figlet Writer Usage Guide: ||")
+    print("                                   ||")
+    print("Prints the list of valid fonts     ||")
+    print("./figlet.py -dict                  ||")
+    print("            -d                     ||")
+    print("                                   ||")
+    print("Set user defined font              ||")
+    print("./figlet.py --font font_name       ||")
+    print("            -f font_name           ||")
+    print("                                   ||")
+    print("Print this usage guide             ||")
+    print("./figlet.py --help                 ||")
+    print("            -h                     ||")
+
+# Check arguments / set mode
+## Random Mode
 if len(sys.argv) == 1:
     # Pick random font
     isRandom = True
+# Check flags
+## Dictionary Mode
+elif sys.argv[1] == "-d" or sys.argv[1] == "--dict":
+    # Print font list
+    print("Dictionary of Figlet Fonts:\n")
+    
+    cols = 10
+    rows = len(fonts) // cols + (1 if len(fonts) % cols > 0 else 0)
+    for row in range(rows):
+        for col in range(cols):
+            index = row * cols + col
+            if index < len(fonts):
+                print(f'{fonts[index]: <15}', end="")
+        print()
+    exit(1)
+## Usage Guide Help Mode
+elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print_help()
+    exit(2)
 elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
     # set user input font
     isRandom = False
 else:
-    print("Invalid usage.")
-    sys.exit(1)
+    print("Invalid usage.\nRun with -h for help.")
+    sys.exit(3)
 
 # Set Figlet options
 if isRandom == False:
@@ -24,8 +60,8 @@ if isRandom == False:
         figlet.setFont(font=font)
     except:
         # handle invalid font
-        print("Invalid font.")
-        sys.exit(1)
+        print("Invalid font.\nRun with -h for help.")
+        sys.exit(4)
 else:
     # Get random font
     font = random.choice(figlet.getFonts())
