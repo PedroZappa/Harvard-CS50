80cc1f0b747adf55aac4b3a66718782e3884e596
PedroZappa <pedrogzappa@gmail.com>
2023-05-25 22:38:15 +0100

    figlet.py fix / comments

--- Code/PracticeProblems/pp6/figlet/figlet.py
+++ Code/PracticeProblems/pp6/figlet/figlet.py
@@ -1,27 +1,41 @@
 import sys, random
 from pyfiglet import Figlet
 
-figlet = Figlet()
 isRandom = None
+figlet = Figlet()
+fonts = figlet.getFonts()
 
-# Check number arguments
+# Check for arguments
 if len(sys.argv) == 1:
+    # Pick random font
     isRandom = True
 elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+    # set user input font
     isRandom = False
 else:
     print("Invalid usage.")
     sys.exit(1)
 
+# Set Figlet options
 if isRandom == False:
     try:
-        figlet.setFont(font=sys.argv[2])
+        # Set user defined font
+        font = sys.argv[2]
+        figlet.setFont(font=font)
     except:
-        print("Invalid usage.")
-        sys.exit()
+        # handle invalid font
+        print("Invalid font.")
+        sys.exit(1)
 else:
+    # Get random font
     font = random.choice(figlet.getFonts())
-
+    # set random font
+    figlet.setFont(font=font)
+    
 # Get user input
-text = input("Input: ")    
+text = input("Input: ")
+
+# Print font meta
+print(f"\nFont: {font}")
+# Print font
 print(f"Output:\n\n{figlet.renderText(text)}")
