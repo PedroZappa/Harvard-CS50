c0c6246d2e4c938a74042503027546e64f98199e
PedroZappa <pedrogzappa@gmail.com>
2023-05-26 11:31:05 +0100

    figlet.py small tweeks

--- Code/PracticeProblems/pp6/figlet/figlet.py
+++ Code/PracticeProblems/pp6/figlet/figlet.py
@@ -3,11 +3,13 @@ from pyfiglet import Figlet
 from colored import fg, bg, attr
 
 isRandom = True
+isColor = True
+# color = {fg('purple')}
 figlet = Figlet()
 fonts = figlet.getFonts()
 
 def main():
-    # Check arguments / check flags / set mode
+    # Check arguments & flags / set execution mode
     ## Random Mode
     if len(sys.argv) == 1:
         # Pick random font
@@ -20,9 +22,18 @@ def main():
     elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
         print_help()
         exit(2)
+    ## User defined font Mode
     elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
         # set user input font
         isRandom = False
+    ## User defined color Mode
+    elif len(sys.argv) == 4 and (sys.argv[3] == "-c" or sys.argv[1] == "--color"):
+        # set user input color
+        isColor = True
+    ## User input color code
+    elif len(sys.argv) == 5:
+        # set user input color
+        isColor = True
     else:
         print("Invalid usage.\nRun with -h for help.")
         sys.exit(3)
@@ -36,6 +47,19 @@ def main():
     # Print to the terminal
     print_font(text, font)
     exit(0)
+
+
+def print_dict():
+    # Print font list
+        print(f"{fg(random.randint(0, 255))}Dictionary of Figlet Fonts:\n")
+        cols = 10
+        rows = len(fonts) // cols + (1 if len(fonts) % cols > 0 else 0)
+        for row in range(rows):
+            for col in range(cols):
+                index = row * cols + col
+                if index < len(fonts):
+                    print(f'{fg(random.randint(0, 255))}{fonts[index]: <15}', end="")
+            print("")
 
 
 def print_help():
@@ -53,18 +77,6 @@ def print_help():
     print("./figlet.py --help                 ||")
     print("            -h                     ||")
 
-
-def print_dict():
-    # Print font list
-        print(f"{fg(random.randint(0, 255))}Dictionary of Figlet Fonts:\n")
-        cols = 10
-        rows = len(fonts) // cols + (1 if len(fonts) % cols > 0 else 0)
-        for row in range(rows):
-            for col in range(cols):
-                index = row * cols + col
-                if index < len(fonts):
-                    print(f'{fg(random.randint(0, 255))}{fonts[index]: <15}', end="")
-            print("")
 
 def set_font():
     # Set Figlet options
@@ -95,7 +107,11 @@ def print_font(text, font):
         print("Output:\n\n")
         print(f"{fg(random.randint(0, 255))}{figlet.renderText(text)}")
     else:
-        print(f"_Output:\n\n{figlet.renderText(text)}")
+        if len(argv) == 3:
+            print(f"{fg(random.randint(0, 255))}_Output:\n\n{figlet.renderText(text)}")
+        else:
+            print(f"_Output:\n\n{figlet.renderText(text)}")
+
 
 # Run
 main()
