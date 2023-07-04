20648836c48c4bc03ebcdd808c32954a4db2c989
PedroZappa <pedrogzappa@gmail.com>
2023-07-04 20:01:51 +0100

    half.c : annotated, colored & deCS50ed

--- Code/PracticeProblems/pp1/half.c
+++ Code/PracticeProblems/pp1/half.c
@@ -1,21 +1,37 @@
 // Calculate your half of a restaurant bill
 // Data types, operations, type casting, return value
-
-#include <cs50.h>
 #include <stdio.h>
 
+// Declare function prototype for 'half'
 float half(float bill, float tax, int tip);
 
 int main(void)
 {
-    float bill_amount = get_float("Bill before tax and tip: ");
-    float tax_percent = get_float("Sale Tax Percent: ");
-    int tip_percent = get_int("Tip percent: ");
+    // Get user inputs
+    float bill_amount;
+    printf("Bill before tax and tip: ");
+    scanf("%f", &bill_amount);
 
-    printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
+    float tax_percent;
+    printf("Sale Tax Percent: ");
+    scanf("%f", &tax_percent);
+
+    int tip_percent;
+    printf("Tip percent: ");
+    scanf("%d", &tip_percent);
+
+    // ANSI escape sequences to color output
+    const char *cyan = "\033[36m";
+    const char *reset = "\033[0m";
+
+    // Print the total amount owed, calculated by calling the 'half' function with the user's inputs
+    printf(
+        "You will owe %s$%.2f%s each!\n", 
+        cyan, half(bill_amount, tax_percent, tip_percent), reset
+    );
 }
 
-// TODO: Complete the function
+// Function definition for 'half'
 float half(float bill, float tax, int tip)
 {
     // Convert tax and tip percentages to decimal values
