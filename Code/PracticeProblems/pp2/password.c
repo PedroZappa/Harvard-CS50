91370055aa779e8fd346d74c5aba2be6f1b87c5f
PedroZappa <pedrogzappa@gmail.com>
2023-07-05 22:37:59 +0100

    password.c : de-CS50'd / colorized / annotated / PasswordState struct

--- Code/PracticeProblems/pp2/password.c
+++ Code/PracticeProblems/pp2/password.c
@@ -2,65 +2,106 @@
 // Practice iterating through a string
 // Practice using the ctype library
 
-#include <cs50.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
-bool valid(string password);
+// ANSI escape sequences to color output
+const char *blackBright = "\033[90m";
+const char *cyan = "\033[36m";
+const char *green = "\033[32m";
+const char *red = "\033[31m";
+const char *yellow = "\033[33m";
+const char *magenta = "\033[35m";
+const char *reset = "\033[0m";
 
+const int MAX_LEN = 30;
+
+typedef struct {
+    bool isLower;
+    bool isUpper;
+    bool isNumber;
+    bool isSymbol;
+    bool valid;
+} PasswordState;
+
+// Function prototypes
+PasswordState valid(char* password);
+
+// Main: Check if a password is valid
 int main(void)
 {
-    string password = get_string("Enter your password: ");
-    if (valid(password))
+    char password[30];
+    printf("%sEnter your password: %s", blackBright, reset);
+    fgets(password, MAX_LEN, stdin);
+
+    // Check that a password has at least one lowercase letter, uppercase letter, number and symbol
+    PasswordState valid_result = valid(password);
+
+    // Print Constrains
+    printf("%sConstrains met%s\n", cyan, reset);
+    printf(
+        "%sLowercase%s && %sUppercase%s && %sNumber%s && %sSymbol%s\n",
+        valid_result.isLower ? green : yellow, reset,
+        valid_result.isUpper ? green : yellow, reset,
+        valid_result.isNumber ? green : yellow, reset,
+        valid_result.isSymbol ? green : yellow, reset
+    );
+
+    // Print valid() result
+    if (valid_result.valid == true)
     {
-        printf("Your password is valid!\n");
+        printf("Your password is %svalid%s\n", green, reset);
     }
     else
     {
-        printf("Your password needs at least one uppercase letter, lowercase letter, number and symbol\n");
+        printf("Your password is %sinvalid%s\n", red, reset);
     }
 }
 
 // TODO: Complete the Boolean function below
-bool valid(string password)
+PasswordState valid(char* password)
 {
-    bool isLower = false;
-    bool isUpper = false;
-    bool isNumber = false;
-    bool isSymbol = false;
+    PasswordState state = {
+        .isLower = false,
+        .isUpper = false,
+        .isNumber = false,
+        .isSymbol = false,
+        .valid = false
+    };
 
-    // Loop through password[elements]
+    // Loop through each password[letter]
     for (int i = 0, n = strlen(password); i < n; i++)
     {
         // if password[i] is lowercase, isLower = true
         if (password[i] >= 'a' && password[i] <= 'z')
         {
-            isLower = true;
+            state.isLower = true;
         }
         // if password[i] is uppercase, isUpper = true
         else if (password[i] >= 'A' && password[i] <= 'Z')
         {
-            isUpper = true;
+            state.isUpper = true;
         }
         // if password[i] is number, isNumber = true
         else if (password[i] >= '0' && password[i] <= '9')
         {
-            isNumber = true;
+            state.isNumber = true;
         }
         // if password[i] is symbol, isSymbol = true
-        else
+        else if (password[i] == '@' || password[i] == '#' || password[i] == '!' || password[i] == '~' || password[i] == '$' || password[i] == '%' || password[i] == '^' || password[i] == '&' || password[i] == '*' || password[i] == '(' || password[i] == ')' || password[i] == '-' || password[i] == '+' || password[i] == '/' || password[i] == ':' || password[i] == '.' || password[i] == ',' || password[i] == '<' || password[i] == '>' || password[i] == '?' || password[i] == '|' || password[i] == '_')
         {
-            isSymbol = true;
+            state.isSymbol = true;
         }
     }
 
-    if (isLower == true && isUpper == true && isNumber == true && isSymbol == true)
+    // Return true if all constrains are met
+    if (state.isLower == true && state.isUpper == true && state.isNumber == true && state.isSymbol == true)
     {
         // Return true if all bools == true
-        return true;
+        state.valid = true;
     }
 
-    return false;
+    return state;
 }
