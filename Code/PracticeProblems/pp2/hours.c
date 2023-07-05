db243cd093184d08a82ebdca3abae5e108e1fc8a
PedroZappa <pedrogzappa@gmail.com>
2023-07-05 17:26:48 +0100

    hours.c : de-CS50'd / colorized / annotated

--- Code/PracticeProblems/pp2/hours.c
+++ Code/PracticeProblems/pp2/hours.c
@@ -1,31 +1,97 @@
-#include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-float calc_hours(int hours[], int weeks, char output);
+// ANSI escape sequences to color output
+const char *blackBright = "\033[90m";
+const char *cyan = "\033[36m";
+const char *green = "\033[32m";
+const char *red = "\033[31m";
+const char *yellow = "\033[33m";
+const char *magenta = "\033[35m";
+const char *reset = "\033[0m";
 
+// Declare function prototypes
+float calc_hours(int *hours, int weeks, char output);
+char print_output(int *hours, int weeks, char output);
+
+// Main: Calculate total or average hours/week
 int main(void)
 {
-    int weeks = get_int("Number of weeks taking CS50: ");
-    int hours[weeks];
+    // Declare user input as a string
+    char input[10];
 
-    for (int i = 0; i < weeks; i++)
-    {
-        hours[i] = get_int("Week %i HW Hours: ", i);
+    // Get number of weeks taking CS50 from user
+    int weeks, result;
+    do {
+        printf(
+            "%sNumber of weeks%s taking %sCS50%s: ",
+            cyan, reset, magenta, reset
+        );
+        fgets(input, sizeof(input), stdin);
+
+        // Try to read an integer from the input
+        result = sscanf(input, "%d", &weeks);
+
+        // If sscanf couldn't read an integer or the number is not positive, print an error message
+        if (result != 1 || weeks <= 0)
+        {
+            printf(
+                "Ups! %sInvalid%s # o'Weeks. Please input a %spositive integer%s!\n",
+                red, reset, green, reset
+            );
+            weeks = 0; // Reset weeks to 0 so the loop continues
+        }
     }
+    while (weeks <= 0);
 
+    // Get hours taken for each week
+    int hours[weeks];
+    do 
+    {   
+        // Loop # times through # o'Weeks
+        for (int i = 0; i < weeks; i++)
+        {
+            do
+            {
+                printf("Week %i HW Hours: ", i);
+                fgets(input, sizeof(input), stdin);
+
+                // Try to read an integer from the input
+                result = sscanf(input, "%d", &hours[i]);   
+
+                // If sscanf couldn't read an integer or the number is not positive, print an error message
+                if (result != 1 || hours[i] < 0)
+                {
+                    printf(
+                        "Ups! %sInvalid%s # of Hours. Please input a %spositive integer%s!\n",
+                        red, reset, green, reset
+                    );
+                }    
+            } while (result != 1 || hours[i] < 0);    
+        } 
+    } while (hours < 0);
+
+    // Select output type
     char output;
     do
     {
-        output = toupper(get_char("Enter T for total hours, A for average hours per week: "));
+        printf(
+            "Enter %sT%s for %stotal%s hours, %sA%s for %saverage%s hours per week: ",
+            green, reset, green, reset, cyan, reset, cyan, reset
+        );
+        scanf(" %c", &output);
+        output = toupper(output);
     }
     while (output != 'T' && output != 'A');
 
-    printf("%.1f hours\n", calc_hours(hours, weeks, output));
+    // Print output
+    print_output(hours, weeks, output);
 }
 
-// TODO: complete the calc_hours function
-float calc_hours(int hours[], int weeks, char output)
+// Calculate Total Hours or Average Hours per Week
+float calc_hours(int *hours, int weeks, char output)
 {
     // total hours
     float total_hours = 0.0;
@@ -50,9 +116,37 @@ float calc_hours(int hours[], int weeks, char output)
             break;
 
         default:
-            printf("Ups! Invalid input\n");
+            printf(
+                "%sUps! Invalid input%s\n", 
+                red, reset
+            );
             break;
     }
 
     return total_hours;
+}
+
+// Print output
+char print_output(int *hours, int weeks, char output)
+{
+    // Print 
+    switch (output)
+    {
+        case 'T':
+            printf(
+                "%s%.1f%s hours\n", 
+                green, calc_hours(hours, weeks, output), reset
+            );
+            break;
+        case 'A':
+            printf(
+                "%s%.1f%s hours\n", 
+                cyan, calc_hours(hours, weeks, output), reset
+            );
+            break;
+        default:
+            break;
+    }
+
+    return 0;
 }
