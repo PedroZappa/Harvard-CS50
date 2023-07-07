fb587baa21fbad29b50e10bc89eaabac28abaa7d
PedroZappa <pedrogzappa@gmail.com>
2023-07-07 21:46:41 +0100

    temps.c : de-CS50'd / colorized / annotated

--- Code/PracticeProblems/pp3/temps.c
+++ Code/PracticeProblems/pp3/temps.c
@@ -1,20 +1,28 @@
 // Practice working with structs
 // Practice applying sorting algorithms
-
-#include <cs50.h>
 #include <stdio.h>
+
+// ANSI color codes for boxed in letters
+#define BLACKWHITE "\e[38;2;255;255;255;1m\e[48;2;0;0;0;1m"
+#define BLACKBRIGHT "\e[90m"
+#define RED "\e[31m"
+#define GREEN "\e[32m"
+#define YELLOW "\e[33m"
+#define CYAN "\e[36m"
+#define RESET "\e[0;39m"
 
 #define NUM_CITIES 10
 
 typedef struct
 {
-    string city;
+    char *city;
     int temp;
 }
 avg_temp;
 
 avg_temp temps[NUM_CITIES];
 
+// Function prototypes
 void sort_cities(void);
 
 int main(void)
@@ -49,37 +57,40 @@ int main(void)
     temps[9].city = "San Francisco";
     temps[9].temp = 66;
 
+    // Sort cities by temperature
     sort_cities();
 
-    printf("\nAverage July Temperatures by City\n\n");
+    printf(BLACKWHITE "Average July Temperatures by City" RESET "\n");
 
     for (int i = 0; i < NUM_CITIES; i++)
     {
-        printf("%s: %i\n", temps[i].city, temps[i].temp);
+        printf(BLACKBRIGHT "%s: " RESET YELLOW " %i\n" RESET, temps[i].city, temps[i].temp);
     }
+
+    return 0;
 }
 
-// TODO: Sort cities by temperature in descending order
+// Sort cities by temperature in descending order
 void sort_cities(void)
 {
     // Bubble sort through temps[]
-    int n = NUM_CITIES;
-    // temp buffer
-    avg_temp temp_buffer;
+    int n = NUM_CITIES; // Number of cities
+    avg_temp temp_buffer; // Temporary buffer for swapping
 
+    // Outer loop for each pass
     for (int i = 0; i < n; i++)
     {
+        // Inner loop for comparisons and swapping
         for (int j = 0; j < n - i - 1; j++)
         {
-            // Check temps[].temp protery
+            // Compare the temperatures of adjacent cities
             if (temps[j].temp < temps[j + 1].temp)
             {
-                temp_buffer = temps[j];
-                temps[j] = temps[j+ 1];
-                temps[j + 1] = temp_buffer;
+                // if the current temp is less than the next temp
+                temp_buffer = temps[j]; // Store the current in buffer
+                temps[j] = temps[j+ 1]; // Overwrite current with next
+                temps[j + 1] = temp_buffer; // Overwrite next with buffered current
             }
         }
     }
-
-
 }
